101f829f5834d228bb26cdd05c489997c6a73e02
Kseniya <nicksi-8610@yandex.ru>
2023-10-06 23:37:14 +0300

    Решены задачи семинра

---
+++ task03/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+3 -> Среда
+5 -> Пятница
+*/
+
+System.Console.WriteLine("Введите число");
+int weekday = Convert.ToInt32(Console.ReadLine());
+
+int day1 = 1;
+int day2 = 2;
+int day3 = 3;
+int day4 = 4;
+int day5 = 5;
+int day6 = 6;
+int day7 = 7;
+
+if (weekday == day1) Console.WriteLine("Понедельник");
+if (weekday == day2) Console.WriteLine("Вторник");
+if (weekday == day3) Console.WriteLine("Среда");
+if (weekday == day4) Console.WriteLine("Четверг");
+if (weekday == day5) Console.WriteLine("Пятница");
+if (weekday == day6) Console.WriteLine("Суббота");
+if (weekday == day7) Console.WriteLine("Воскресенье");
+
---
+++ task05/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+5. Напишите программу, которая на вход принимает одно число (N), 
+а на выходе показывает все целые числа в промежутке от -N до N. 
+
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+2 -> " -2, -1, 0, 1, 2"
+*/
+
+System.Console.WriteLine("введите натуральное число ");
+int numN = Convert.ToInt32(Console.ReadLine());
+for (int i = - numN; i <= numN; i++)
+{
+    Console.Write(i);
+    if(i<numN)
+    System.Console.Write(", ");
+}
