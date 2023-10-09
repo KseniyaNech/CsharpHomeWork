2b13b0248cda200f0ed94feec4375f77898352f6
Kseniya <nicksi-8610@yandex.ru>
2023-10-09 11:58:33 +0300

    Решение домашнего задания для самопроверки

---
+++ task02/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Внутри класса Answer напишите метод CompareNumbers, который на вход принимает два числа и выводит, 
+какое число большее, а какое меньшее.
+a = 5; b = 7 -> Первое число 5 меньше чем второе число 7
+a = 2; b = 2 -> Введенные числа равны 2
+*/
+
+System.Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    System.Console.WriteLine(number1 + " больше чем " + number2);
+}
+else if (number2>number1)
+{
+    System.Console.WriteLine(number2 + " больше чем " + number1);
+}
+else if (number1 == number2)
+{
+    System.Console.WriteLine(number1 + " равно " + number2);
+}
---
+++ task08/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Внутри класса Answer напишите метод PrintEvenNumbers, которая на вход принимает число (number), 
+а на выходе выводит все чётные числа от 1 до number (включительно), 
+разделеные знаком табуляции.
+*/
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i < number; i++) //Всегда три показателя: точка входа; условие выхода из цикла; шаг
+{
+    System.Console.Write(i + "\t");
+}
