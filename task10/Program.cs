1d87e8df536eff837cf512b5a0e7c613ff47c6ec
Kseniya <nicksi-8610@yandex.ru>
2023-10-12 19:30:30 +0300

    Переименования тасков

---
+++ task10/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int num = new Random().Next(100, 1000);
+
+System.Console.WriteLine("Сегенерировано трехзначное число: " + num);
+
+int temp = num % 10;
+
+num /= 100;
+num = num * 10 + temp;
+
+System.Console.Write("Удаляем вторую цифру: ");
+System.Console.WriteLine(num);
